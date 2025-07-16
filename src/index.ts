82446f2116132e45048a02506536f7926db77ec5
TheWrecker <36756657+Joshd150@users.noreply.github.com>
2025-07-16 15:22:30 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import app from "./server"
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3002
 app.listen(port, () => {
   console.log(`server started on ${port}`);
 });
